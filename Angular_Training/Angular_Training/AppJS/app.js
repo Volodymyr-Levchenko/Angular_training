aecd249f5435647553b1dfcf874763feb8e47e6d
Volodymyr-Levchenko <dante70850@gmail.com>
2014-09-02 13:42:11 +0300

    Added menu strip on left

--- Angular_Training/Angular_Training/AppJS/app.js
+++ Angular_Training/Angular_Training/AppJS/app.js
@@ -1,24 +1,36 @@
-﻿angular.module('NoteBook', [])
-.controller('BookController', function() {
+﻿var app = angular.module('NoteBook', []);
+
+app.controller('BookController', function() {
     this.notes = myNotes;
-})
-.directive('noteList', function() {
-    return {
-        restrict: 'E',
-        templateUrl:"/Templates/_noteTemplate.html"
-    };
+
 });
+app.directive('noteList', function() {
+        return {
+            restrict: 'E',
+            templateUrl: "/Templates/_noteTemplate.html"
+        };
+});
+app.controller('StripController', function() {
+    this.cats = categories;
+});
+var categories = {
+    WORK: { name: "Work", id: "0" },
+    HOME: { name: "Home", id: "1" }
+};
 var myNotes = [
-    {
-        header: 'TestNote1',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget mi sit amet nisi vestibulum consequat. Nullam tempor viverra nunc id ultrices. Quisque scelerisque turpis id orci laoreet, non molestie nulla vehicula. Nullam purus odio, mollis venenatis tortor sit amet, lobortis rhoncus turpis. Ut faucibus bibendum neque, a feugiat urna. Pellentesque nisl risus, tincidunt eleifend iaculis non, commodo eget arcu. Nunc ac pretium magna, vel molestie tellus.'
-    },
-    {
-        header: 'TestNote2',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget mi sit amet nisi vestibulum consequat. Nullam tempor viverra nunc id ultrices. Quisque scelerisque turpis id orci laoreet, non molestie nulla vehicula. Nullam purus odio, mollis venenatis tortor sit amet, lobortis rhoncus turpis. Ut faucibus bibendum neque, a feugiat urna. Pellentesque nisl risus, tincidunt eleifend iaculis non, commodo eget arcu. Nunc ac pretium magna, vel molestie tellus.'
-    },
-    {
-        header: 'TestNote3',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget mi sit amet nisi vestibulum consequat. Nullam tempor viverra nunc id ultrices. Quisque scelerisque turpis id orci laoreet, non molestie nulla vehicula. Nullam purus odio, mollis venenatis tortor sit amet, lobortis rhoncus turpis. Ut faucibus bibendum neque, a feugiat urna. Pellentesque nisl risus, tincidunt eleifend iaculis non, commodo eget arcu. Nunc ac pretium magna, vel molestie tellus.'
-    }
+{
+    header: 'TestNote1',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget mi sit amet nisi vestibulum consequat. Nullam tempor viverra nunc id ultrices. Quisque scelerisque turpis id orci laoreet, non molestie nulla vehicula. Nullam purus odio, mollis venenatis tortor sit amet, lobortis rhoncus turpis. Ut faucibus bibendum neque, a feugiat urna. Pellentesque nisl risus, tincidunt eleifend iaculis non, commodo eget arcu. Nunc ac pretium magna, vel molestie tellus.',
+    category: categories.WORK
+},
+{
+    header: 'TestNote2',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget mi sit amet nisi vestibulum consequat. Nullam tempor viverra nunc id ultrices. Quisque scelerisque turpis id orci laoreet, non molestie nulla vehicula. Nullam purus odio, mollis venenatis tortor sit amet, lobortis rhoncus turpis. Ut faucibus bibendum neque, a feugiat urna. Pellentesque nisl risus, tincidunt eleifend iaculis non, commodo eget arcu. Nunc ac pretium magna, vel molestie tellus.',
+    category:categories.WORK
+},
+{
+    header: 'TestNote3',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget mi sit amet nisi vestibulum consequat. Nullam tempor viverra nunc id ultrices. Quisque scelerisque turpis id orci laoreet, non molestie nulla vehicula. Nullam purus odio, mollis venenatis tortor sit amet, lobortis rhoncus turpis. Ut faucibus bibendum neque, a feugiat urna. Pellentesque nisl risus, tincidunt eleifend iaculis non, commodo eget arcu. Nunc ac pretium magna, vel molestie tellus.',
+    category:categories.HOME
+}
 ];
