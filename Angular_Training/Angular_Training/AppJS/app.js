548a8cdbeccdff88f7d7aa7947c9c6c2a62d57b4
Volodymyr-Levchenko <dante70850@gmail.com>
2014-09-03 23:10:32 +0300

    implemented basic routing

--- Angular_Training/Angular_Training/AppJS/app.js
+++ Angular_Training/Angular_Training/AppJS/app.js
@@ -1,30 +1,49 @@
-﻿var app = angular.module('NoteBook', []);
+﻿var app = angular.module('NoteBook',
+    ['ngRoute']
+    );
+app.config([
+    '$routeProvider', function($routeProvider) {
+        $routeProvider.
+            when('/book/notes/:noteid', {
+                templateUrl: '/Templates/_noteTemplate.html',
+                controller: 'NoteController'
+            }).
+            otherwise({
+                redirectTo:'/book/notes/0'
+            });
 
+    }
+]);
+app.controller('NoteController', [
+    '$scope', '$routeParams', function ($scope, $routeParams) {
+        $scope.note = fetchNote($routeParams.noteid);
+    }
+]);
 app.controller('BookController', function() {
     this.notes = myNotes;
-    this.currentNote = this.notes[0];
 });
-app.directive('displayNote', function() {
-    return {
-            scope: {
-              ngModel:'='  
-            },
-            restrict: 'E',
-            templateUrl: "/Templates/_noteTemplate.html",
-            controller:function() {
-                this.setModel = function(scope, note) {
-                    scope.ngModel = note;
-                    scope.$apply();
-                };
-            },
-            controllerAs:"display"
-        };
-});
-app.controller('StripController', function() {
-    this.cats = categories;
-    this.noteHeadersVisible = false;
-    this.toggleHeaderVisibility= function(showParam) {
-        this.noteHeadersVisible = showParam;
+
+//app.directive('displayNote', function() {
+//    return {
+//            scope: {
+//              ngModel:'='  
+//            },
+//            restrict: 'E',
+//            templateUrl: "/Templates/_noteTemplate.html",
+//            controller:function() {
+//                this.setModel = function(scope, note) {
+//                    scope.ngModel = note;
+//                    scope.$apply();
+//                };
+//            },
+//            controllerAs:"display"
+//        };
+//});
+app.controller('StripController', function($scope) {
+    $scope.cats = categories;
+    $scope.noteHeadersVisible = false;
+    $scope.toggleHeaderVisibility = function (showParam) {
+        $scope.noteHeadersVisible = showParam;
     };
 });
 var categories = {
@@ -33,18 +52,29 @@ var categories = {
 };
 var myNotes = [
 {
+    id:0,
     header: 'TestNote1',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget mi sit amet nisi vestibulum consequat. Nullam tempor viverra nunc id ultrices. Quisque scelerisque turpis id orci laoreet, non molestie nulla vehicula. Nullam purus odio, mollis venenatis tortor sit amet, lobortis rhoncus turpis. Ut faucibus bibendum neque, a feugiat urna. Pellentesque nisl risus, tincidunt eleifend iaculis non, commodo eget arcu. Nunc ac pretium magna, vel molestie tellus.',
     category: categories.WORK
 },
 {
+    id:1,
     header: 'TestNote2',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget mi sit amet nisi vestibulum consequat. Nullam tempor viverra nunc id ultrices. Quisque scelerisque turpis id orci laoreet, non molestie nulla vehicula. Nullam purus odio, mollis venenatis tortor sit amet, lobortis rhoncus turpis. Ut faucibus bibendum neque, a feugiat urna. Pellentesque nisl risus, tincidunt eleifend iaculis non, commodo eget arcu. Nunc ac pretium magna, vel molestie tellus.',
     category:categories.WORK
 },
 {
+    id:2,
     header: 'TestNote3',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget mi sit amet nisi vestibulum consequat. Nullam tempor viverra nunc id ultrices. Quisque scelerisque turpis id orci laoreet, non molestie nulla vehicula. Nullam purus odio, mollis venenatis tortor sit amet, lobortis rhoncus turpis. Ut faucibus bibendum neque, a feugiat urna. Pellentesque nisl risus, tincidunt eleifend iaculis non, commodo eget arcu. Nunc ac pretium magna, vel molestie tellus.',
     category:categories.HOME
 }
 ];
+var fetchNote = function (id) {
+    var i;
+    var res=null;
+    for(i=0;i<myNotes.length;i++)
+        if (myNotes[i].id == id)
+            res = myNotes[i];
+    return res;
+}
