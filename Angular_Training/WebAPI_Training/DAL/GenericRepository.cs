3a17846d8bb05b688749819c65cfcec9f81cface
Volodymyr-Levchenko <dante70850@gmail.com>
2014-09-08 00:24:58 +0300

    backend: db, ado.net model, repository & unit of work

---
+++ Angular_Training/WebAPI_Training/DAL/GenericRepository.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using WebAPI_Training.Models;
+
+namespace WebAPI_Training.DAL
+{
+    public class GenericRepository<TEntity> where TEntity:class
+    {
+        internal NoteModelContainer _container;
+        internal DbSet<TEntity> _set;
+
+        public GenericRepository(NoteModelContainer containerParam)
+        {
+            _container = containerParam;
+            _set = _container.Set<TEntity>();
+        }
+
+        public virtual IEnumerable<TEntity> Get(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = "")
+        {
+            IQueryable<TEntity> query = _set;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new [] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            return orderBy != null ? orderBy(query).ToList() : query.ToList();
+        }
+
+        public virtual TEntity GetById(object id)
+        {
+            return _set.Find(id);
+        }
+
+        public virtual void Insert(TEntity entity)
+        {
+            _set.Add(entity);
+        }
+
+        public virtual void Delete(object id)
+        {
+            var entityToDelete = _set.Find(id);
+            Delete(entityToDelete);
+        }
+
+        public virtual void Delete(TEntity entityToDelete)
+        {
+            if (_container.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                _set.Attach(entityToDelete);
+            }
+            _set.Remove(entityToDelete);
+        }
+
+        public virtual void Update(TEntity entityToUpdate)
+        {
+            _set.Attach(entityToUpdate);
+            _container.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+    }
+}
---
+++ Angular_Training/WebAPI_Training/DAL/UnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAPI_Training.Models;
+
+namespace WebAPI_Training.DAL
+{
+    public class UnitOfWork:IDisposable
+    {
+        private NoteModelContainer _container;
+        private GenericRepository<Note> noteRepository;
+        private GenericRepository<Category> categoryRepository;
+
+        public GenericRepository<Note> NoteRepository
+        {
+            get { return noteRepository ?? new GenericRepository<Note>(_container); }
+        }
+
+        public GenericRepository<Category> CategoryRepository
+        {
+            get { return categoryRepository ?? new GenericRepository<Category>(_container); }
+        }
+
+        public void Save()
+        {
+            _container.SaveChanges();
+        }
+
+        private bool disposed;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    _container.Dispose();
+                }
+            }
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
