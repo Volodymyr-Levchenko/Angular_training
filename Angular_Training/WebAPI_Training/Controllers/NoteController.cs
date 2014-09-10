910c3f0b37e224ededc9e7cd8310acaa0dc6ffd1
Volodymyr-Levchenko <dante70850@gmail.com>
2014-09-10 22:35:02 +0300

    temp shelve

---
+++ Angular_Training/WebAPI_Training/Controllers/NoteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI_Training.DAL;
+
+namespace WebAPI_Training.Controllers
+{
+    public class NoteController : ApiController
+    {
+        private UnitOfWork _unit = new UnitOfWork();
+
+
+    }
+}
--- Angular_Training/WebAPI_Training/DAL/GenericRepository.cs
+++ Angular_Training/WebAPI_Training/DAL/GenericRepository.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Web;
 using WebAPI_Training.Models;
 
 namespace WebAPI_Training.DAL
